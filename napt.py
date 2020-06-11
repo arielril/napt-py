ba2f57532e3904e91dde03d99f1764a58db0a2f2
Ariel Ril <ariel.ril@4all.com>
2020-06-11 18:19:13 -0300

    add real mapping for ping

--- napt.py
+++ napt.py
@@ -1,3 +1,5 @@
+import typing
+
 from util.format import bytes_to_mac
 import queue
 from queue import Queue
@@ -25,41 +27,116 @@ ETH_P_IP = 0x0800
 ETH_LEN = 14
 
 
-napt_table = list(dict({
-    "protocol": "",
-    "ip_src": "",
-    "ip_dst": "",
-    "port_src": "",
-    "port_dst": "",
-}))
+# napt_table = list(dict({
+#     "protocol": "",
+#     "ip_src": "",
+#     "ip_dst": "",
+#     "port_src": "",
+#     "port_dst": "",
+# }))
 
-
-def getSocketByProto(proto: int = socket.getprotobyname('icmp')) -> socket:
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, proto)
-    s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-    return s
+"""
+    protocol: str
+    ip_src: str
+    ip_dst: str
+    icmp_pid: int | None
+    imcp_seqid: int | None
+"""
+db = list()
 
 
 def isIP(eth) -> bool:
     return eth[2] == 2048
 
 
-# def getWritableSocketFromPacket(packet):
-#     wrt_sock = getSocketByProto(socket.getprotobyname('icmp'))
-#     return wrt_sock
+def isIcmp(ip_packet: bytes) -> bool:
+    ip_unpack = struct.unpack(
+        '!BBHHHBBH4s4s', ip_packet[:20])
+
+    return ip_unpack[6] == 1
 
 
-def showPacket(packet):
-    eth_header = packet[:ETH_LEN]
-    eth = struct.unpack('!6s6sH', eth_header)
+def getPacketWithRedirect(old_pkt: bytes, to_ip_src: str, to_ip_dst: str) -> bytes:
+    rest_pkt = old_pkt[20:]
+    ip_unpack = struct.unpack(
+        '!BBHHHBBH4s4s', old_pkt[:20])
 
-    print('MAC Src: '+bytes_to_mac(eth[1]))
-    print('MAC Dst: '+bytes_to_mac(eth[0]))
-    print('Type: '+hex(eth[2]))
-    print('{0}'.format(eth[2]))
+    if to_ip_src == None:
+        to_ip_src = socket.inet_ntoa(ip_unpack[8])
+
+    ip_saddr = socket.inet_aton(to_ip_src)
+    ip_daddr = socket.inet_aton(to_ip_dst)
+
+    newPkt = struct.pack(
+        '!BBHHHBBH4s4s',
+        ip_unpack[0],
+        ip_unpack[1],
+        ip_unpack[2],
+        ip_unpack[3],
+        ip_unpack[4],
+        ip_unpack[5],
+        ip_unpack[6],
+        ip_unpack[7],
+        ip_saddr,
+        ip_daddr,
+    )
+
+    return newPkt+rest_pkt
+
+
+def addRedirected(protocol: str, ip_src: str, ip_dst: str, icmp_pid: int = None, icmp_seqid: int = None):
+    dbval = dict({
+        'protocol': protocol,
+        'ip_src': ip_src,
+        'ip_dst': ip_dst,
+        'icmp_pid': icmp_pid,
+        'icmp_seqid': icmp_seqid,
+    })
+    db.append(dbval)
+
+
+def addRedirectedICMP(ip_src: str, ip_dst: str, ip_packet: bytes):
+    unpack = struct.unpack('!BBHHH', ip_packet[20:28])
+    icmp_pid = unpack[3]
+    icmp_seqid = unpack[4]
+
+    addRedirected('icmp', ip_src, ip_dst, icmp_pid, icmp_seqid)
+
+
+def getInternalRequestFromExtPacket(ip_packet: bytes) -> dict:
+    ip_unpack = struct.unpack('!BBHHHBBH4s4s', ip_packet[:20])
+    priv_ip_src_pkt = None
+    # ! IP_SRC PARAM FROM PUB NET
+    priv_ip_dest_pkt = socket.inet_ntoa(ip_unpack[8])
+    icmp_pid = None
+    icmp_seqid = None
+    protocol = None
+
+    if isIcmp(ip_packet):
+        icmp_unpack = struct.unpack('!BBHHH', ip_packet[20:28])
+        icmp_pid = icmp_unpack[3]
+        icmp_seqid = icmp_unpack[4]
+        protocol = 'icmp'
+
+    result = None
+
+    for val in db:
+        if val['protocol'] == protocol \
+                and val['ip_dst'] == priv_ip_dest_pkt \
+                and val['icmp_pid'] == icmp_pid \
+                and val['icmp_seqid'] == icmp_seqid:
+            result = val
+            break
+
+    if result:
+        # consume the response
+        db.remove(val)
+
+    return result
 
 
 if __name__ == '__main__':
+    print('running....')
     # Create 2 sockets, one for each interface eth0 and eth1
     try:
         sniff_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,
@@ -69,12 +146,10 @@ if __name__ == '__main__':
                                   socket.ntohs(ETH_P_IP))
         ext_sniff.bind(('eth1', 0))
     except OSError as msg:
-        print('Error'+str(msg))
+        print('failed to create the sniffer socket'+str(msg))
         sys.exit(1)
 
-    print('Sockets created!')
-
-    inputs = [sniff_socket, ext_sniff]
+    inputs = [sniff_socket]
     outputs = []
     message_queues = {}
 
@@ -83,75 +158,100 @@ if __name__ == '__main__':
             inputs, outputs, inputs)
 
         for s in readable:
-            packet = s.recv(2048)
-            print('received', packet)
+            (packet, addr) = s.recvfrom(65536)
+
+            if not packet and not s is sniff_socket:
+                if s in outputs:
+                    outputs.remove(s)
+                inputs.remove(s)
+                s.close()
+
+                del message_queues[s]
+
             eth_header = packet[:ETH_LEN]
 
             eth = struct.unpack('!6s6sH', eth_header)
 
-            nexthdr = packet[ETH_LEN:]
+            ip_packet = packet[ETH_LEN:]
 
-            if isIP(eth):
-                if s is sniff_socket:
-                    deliv_socket = socket.socket(
+            if s is sniff_socket:
+
+                # * Handle ICMP "connections"
+                if isIP(eth):
+
+                    if isIcmp(ip_packet):
+                        nsock = socket.socket(
+                            socket.AF_INET, socket.SOCK_RAW, socket.htons(ETH_P_IP))
+                        nsock.setsockopt(
+                            socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+                        ip_unpack = struct.unpack(
+                            '!BBHHHBBH4s4s', ip_packet[:20])
+
+                        ip_source = socket.inet_ntoa(ip_unpack[8])  # PRIV NET
+                        ip_dest = socket.inet_ntoa(ip_unpack[9])  # PUB NET
+
+                        if nsock not in message_queues.keys():
+                            message_queues[nsock] = Queue()
+
+                        addRedirectedICMP(ip_source, ip_dest, ip_packet)
+                        msg = {
+                            'pkt': ip_packet,
+                            'dest': (ip_dest, 0),
+                            'redirect': True,
+                            'set_dest': False,
+                        }
+
+                        message_queues[nsock].put(msg)
+                        inputs.append(ext_sniff)
+                        outputs.append(nsock)
+
+                    else:
+                        # * handle TCP and UDP
+                        print('TCP bitcheee')
+
+            else:
+                # * not the initial connection
+
+                if isIP(eth):
+                    # disable external sniffer
+                    inputs.remove(ext_sniff)
+
+                    nsock = socket.socket(
                         socket.AF_INET, socket.SOCK_RAW, socket.htons(ETH_P_IP))
-                    deliv_socket.setsockopt(
-                        socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-                    deliv_socket.bind(('10.0.1.1', 0))
+                    nsock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-                    ip_unpack = struct.unpack(
-                        '!BBHHHBBH4s4s', packet[ETH_LEN:20+ETH_LEN])
-                    ip_dest = socket.inet_ntoa(ip_unpack[9])  # PUB NET
+                    fromDB = getInternalRequestFromExtPacket(ip_packet)
 
-                    if deliv_socket not in message_queues.keys():
-                        message_queues[deliv_socket] = Queue()
+                    if not fromDB:
+                        continue
 
-                    message_queues[deliv_socket].put({
-                        'pkt': nexthdr,
+                    if nsock not in message_queues.keys():
+                        message_queues[nsock] = Queue()
+
+                    ip_dest = fromDB['ip_src']
+                    msg = {
+                        'pkt': ip_packet,
                         'dest': (ip_dest, 0),
-                    })
-                    outputs.append(deliv_socket)
+                        'redirect': False,
+                        'set_dest': True,
+                    }
+                    message_queues[nsock].put(msg)
 
-                elif s is ext_sniff:
-                    deliv_socket = socket.socket(
-                        socket.AF_INET, socket.SOCK_RAW, socket.htons(ETH_P_IP))
-                    deliv_socket.setsockopt(
-                        socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-                    deliv_socket.bind(('10.0.0.1', 0))
-
-                    ip_unpack = struct.unpack(
-                        '!BBHHHBBH4s4s', packet[ETH_LEN:20+ETH_LEN])
-                    ip_dest = socket.inet_ntoa(ip_unpack[9])  # PRIV NET
-
-                    if deliv_socket not in message_queues.keys():
-                        message_queues[deliv_socket] = Queue()
-
-                    message_queues[deliv_socket].put({
-                        'pkt': nexthdr,
-                        'dest': (ip_dest, 0),
-                    })
-
-                    outputs.append(deliv_socket)
-
-                else:
-                    print('not sniff')
-                    # Header Ethernet
-                    # MAC Destino - 6 bytes
-                    dest_mac = b'\x00\x00\x00\xaa\x00\x00'
-                    # MAC Origem - 6 bytes
-                    source_mac = b'\x00\x00\x00\xaa\x00\x01'
-                    protocol = eth[2]
-
-                    eth_hdr = struct.pack(
-                        '!6s6sH', dest_mac, source_mac, protocol)
-
-                    packet = eth_hdr+nexthdr
-                    sniff_socket.send(packet)
+                    outputs.append(nsock)
 
         for w in writable:
             try:
                 msg = message_queues[w].get_nowait()
-            except queue.Empty:
+
+                if msg['redirect']:
+                    msg['pkt'] = getPacketWithRedirect(
+                        msg['pkt'], '10.0.1.1', msg['dest'][0])
+                if msg['set_dest']:
+                    msg['pkt'] = getPacketWithRedirect(
+                        msg['pkt'], None, msg['dest'][0])
+
+            except queue.Empty as e:
                 # no more messages from w
                 outputs.remove(w)
             else:
